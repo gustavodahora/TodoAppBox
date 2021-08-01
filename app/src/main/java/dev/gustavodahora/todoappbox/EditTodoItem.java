ea925801b3e1ed8ee6fb376742f6279b96a8cb28
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-07-31 22:28:15 -0300

    Adding new layouts: todo_item, activity_main

---
+++ app/src/main/java/dev/gustavodahora/todoappbox/EditTodoItem.java
@@ -0,0 +1,15 @@
+
+package dev.gustavodahora.todoappbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditTodoItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_todo_item);
+    }
+}
---
+++ app/src/main/java/dev/gustavodahora/todoappbox/ViewTodoItem.java
@@ -0,0 +1,14 @@
+package dev.gustavodahora.todoappbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewTodoItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_todo_item);
+    }
+}
